470ec30800ce9004575b4ff3bb83ce33da4e706b
TUSHAR MISHRA <mishratushar8890@gmail.com>
2024-03-29 23:01:35 +0530

    added routes

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"os"
 
 	"github.com/MISHRA-TUSHAR/restaurant-management-backend/database"
-	"github.com/MISHRA-TUSHAR/restaurant-management-backend/middleware"
-	"github.com/MISHRA-TUSHAR/restaurant-management-backend/routes"
+	middleware "github.com/MISHRA-TUSHAR/restaurant-management-backend/middleware"
+	routes "github.com/MISHRA-TUSHAR/restaurant-management-backend/routes"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func main() {
 	port := os.Getenv("PORT")
@@ -19,5 +21,15 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
 
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
 }
